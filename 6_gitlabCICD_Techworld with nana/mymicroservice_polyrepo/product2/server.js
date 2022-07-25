276c7fcbd39231b77669021c3d00be2395584124
gu <guying005@googlemail.com>
2022-07-25 16:29:24 +0200

    add gitlab cicd course notiz

---
+++ 6_gitlabCICD_Techworld with nana/mymicroservice_polyrepo/product2/server.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let app = express();
+
+
+app.get('/', function (req, res) {
+    res.json({ 
+        products: [{
+            name: "laptop", 
+            price: 500
+        }, {
+            name: "monitor",
+            price: 100
+        }, {
+            name: "keyboard",
+            price: 30
+        }]
+    });
+    res.end();
+});
+
+app.listen(3001, function () {
+  console.log("app listeninddddg ddon port 3001!!!");
+});
+
+
